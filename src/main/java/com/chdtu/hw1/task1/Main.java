57b0871849b45942bc051161db4d1d7cfeb4075f
Sashaa7 <Sashaa7>
2023-12-06 13:27:01 +0200

    hw1 task1

---
+++ src/main/java/com/chdtu/hw1/task1/Main.java
@@ -0,0 +1,8 @@
+package com.chdtu.hw1.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        // виводимо на екран заданий текст
+        System.out.println("Студент дуже розумний");
+    }
+}
