089d78060556eece095a2efabad316db8ca41b44
Sashaa7 <Sashaa7>
2023-12-06 14:17:24 +0200

    hw1 task3

---
+++ src/main/java/com/chdtu/hw1/task3/Main.java
@@ -0,0 +1,9 @@
+package com.chdtu.hw1.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        // оголошення та ініціалізація змінних a і b
+        int a = 5;
+        int b = 10;
+    }
+}
