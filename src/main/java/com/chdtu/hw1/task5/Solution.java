3f943f5bf26aefd02e8019878be14d992ad40076
Sashaa7 <Sashaa7>
2023-12-06 14:30:51 +0200

    hw1 task5

--- src/main/java/com/chdtu/hw1/task5/Solution.java
+++ src/main/java/com/chdtu/hw1/task5/Solution.java
@@ -1,4 +1,17 @@
 package com.chdtu.hw1.task5;
 
 public class Solution {
+    public static void main(String[] args) {
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        System.out.print("Happy New");
+        System.out.println(" Year");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        // System.out.print(" ");
+        // System.out.println("Year");
+    }
 }
+
