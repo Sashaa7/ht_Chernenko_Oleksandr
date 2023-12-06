da563af7f6c41b7d419209a192e0484dad85a37c
Sashaa7 <Sashaa7>
2023-12-06 13:30:30 +0200

    hw1 task2

---
+++ src/main/java/com/chdtu/hw1/task2/Main.java
@@ -0,0 +1,14 @@
+package com.chdtu.hw1.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        // оголошення змінних
+        String name;
+        int age;
+        String city;
+        //Ініціалізація змінних
+        name = "John";
+        age = 25;
+        city = "New York";
+    }
+}
